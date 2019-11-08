541f57cfd5597ace435babf60bd703a44bbfe2c8
nuagedan100 <stephanelebon1983@gmail.com>
2019-11-08 10:37:09 +0100

    Add logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  link = 'asset/images/';
+  link = 'assets/images/';
   title = 'Ticket Incident';
   items = ['Home','Message','Ticket','Contact'];
   login ='';
