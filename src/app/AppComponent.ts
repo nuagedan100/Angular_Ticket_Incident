3187bf69d4caa55642fa5f4fc1f35c73f9f2a4b0
nuagedan100 <stephanelebon1983@gmail.com>
2019-11-13 07:19:25 +0100

    update css

---
+++ src/app/AppComponent.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  link = 'assets/images/';
+  title = 'Ticket Incident';
+  items = ['Home', 'Message', 'Ticket', 'Contact'];
+  login = '';
+  password = '';
+  // functions
+  onClickedContact() {
+    alert(this.login);
+  }
+  onClickedHome() {
+    alert('Home');
+  }
+  onSubmit(event: any) {
+    console.log(this.login);
+    return this.login;
+  }
+  control() {
+    if (this.login.length > 6) {
+      alert('mot de pass surperieur a 6');
+    }
+  }
+}
